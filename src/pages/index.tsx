d4d99ac02af383f517f79454a9983ce217af95bc
Pedro Shin <pedro.shin@flashapp.com.br>
2023-04-08 04:23:01 -0300

    create basic form to create event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,51 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { FormEvent } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useForm } from "react-hook-form";
 
 export default function Home() {
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(e);
+  const onSubmit = async (formData: any) => {
+    console.log(formData.name);
+
+    const postData = async () => {
+      const data = {
+        name: formData.name,
+      };
+
+      try {
+        const response = await fetch(
+          "http://cambry.sa-east-1.elasticbeanstalk.com/events",
+          // "http://localhost:3009/events",
+          {
+            method: "POST",
+            body: JSON.stringify(data),
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        return response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    await postData().then((data) => {
+      alert(data?.message);
+    });
   };
+
+  const { register, handleSubmit } = useForm();
 
   return (
     <main>
-      <form onSubmit={(e) => onSubmit(e)}>
-        <input placeholder="name" />
-        <button type="submit">Submit</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Item Name:
+          <input {...register("name")} type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
       </form>
     </main>
   );
