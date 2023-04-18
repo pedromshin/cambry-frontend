6f6ca9d9d13ac79336b5f65ec26f7e0b54e91f04
Pedro Shin <pedro.shin@flashapp.com.br>
2023-04-18 11:30:07 -0300

    implements with textract

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/ocr-poc/index.tsx
+++ src/pages/ocr-poc/index.tsx
@@ -1,18 +1,49 @@
-import { useEffect, useRef, useState } from "react";
-import { Group, Stack, Text, Image, Progress, Button } from "@mantine/core";
+import { useState } from "react";
+import { Group, Stack, Text, Image, Button } from "@mantine/core";
 import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 
-import {
-  RekognitionClient,
-  DetectTextCommand,
-} from "@aws-sdk/client-rekognition";
+import { AnalyzeDocumentCommand } from "@aws-sdk/client-textract";
+import { TextractClient } from "@aws-sdk/client-textract";
 
-import AWS from "aws-sdk";
+interface Block {
+  Id: string;
+  BlockType: string;
+  EntityTypes: string[];
+  Relationships?: Relationship[];
+  SelectionStatus?: string;
+  Text?: string;
+}
+
+interface Relationship {
+  Type: string;
+  Ids: string[];
+}
+
+interface KeyMap {
+  [blockId: string]: Block;
+}
+
+interface ValueMap {
+  [blockId: string]: Block;
+}
+
+interface BlockMap {
+  [blockId: string]: Block;
+}
+
+const client = new TextractClient({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
+});
 
 const Home = () => {
   const [imageData, setImageData] = useState<{ Bytes: Uint8Array }>();
   const [imageUrl, setImageUrl] = useState<string>();
-  const [ocrResult, setOcrResult] = useState<string | undefined>("");
+  const [ocrResult, setOcrResult] =
+    useState<{ field: string; value: string }[]>();
 
   const readAsArrayBuffer = (file: File) => {
     return new Promise((resolve, reject) => {
@@ -32,32 +63,100 @@ const Home = () => {
     });
   };
 
-  const config = new AWS.Config({
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  });
-
-  const client = new RekognitionClient({
-    region: "us-east-1",
-    credentials: config.credentials as any,
-  });
-
   const params = {
     Image: imageData,
+    Document: imageData,
+    FeatureTypes: ["TABLES", "FORMS", "SIGNATURES"],
   };
 
+  function get_kv_map(response: any): [KeyMap, ValueMap, BlockMap] {
+    const blocks = response.Blocks;
+
+    // get key and value maps
+    const key_map: KeyMap = {};
+    const value_map: ValueMap = {};
+    const block_map: BlockMap = {};
+    for (const block of blocks) {
+      const block_id = block.Id;
+      block_map[block_id] = block;
+      if (block.BlockType == "KEY_VALUE_SET") {
+        if (block.EntityTypes.includes("KEY")) {
+          key_map[block_id] = block;
+        } else {
+          value_map[block_id] = block;
+        }
+      }
+    }
+
+    return [key_map, value_map, block_map];
+  }
+
+  function get_kv_relationship(
+    key_map: KeyMap,
+    value_map: ValueMap,
+    block_map: BlockMap
+  ): Record<string, string[]> {
+    const kvs: Record<string, string[]> = {};
+    for (const [block_id, key_block] of Object.entries(key_map)) {
+      const value_block = find_value_block(key_block, value_map);
+      const key = get_text(key_block, block_map);
+      const val = get_text(value_block, block_map);
+      kvs[key] = [...(kvs[key] || []), val];
+    }
+    return kvs;
+  }
+
+  function find_value_block(key_block: Block, value_map: ValueMap): Block {
+    for (const relationship of key_block.Relationships || []) {
+      if (relationship.Type == "VALUE") {
+        for (const value_id of relationship.Ids) {
+          const value_block = value_map[value_id];
+          return value_block;
+        }
+      }
+    }
+    throw new Error("No value block found");
+  }
+
+  function get_text(result: Block, blocks_map: BlockMap): string {
+    let text = "";
+    if (result.Relationships) {
+      for (const relationship of result.Relationships) {
+        if (relationship.Type == "CHILD") {
+          for (const child_id of relationship.Ids) {
+            const word = blocks_map[child_id];
+            if (word.BlockType == "WORD") {
+              text += word.Text + " ";
+            }
+            if (word.BlockType == "SELECTION_ELEMENT") {
+              if (word.SelectionStatus == "SELECTED") {
+                text += "X ";
+              }
+            }
+          }
+        }
+      }
+    }
+    return text;
+  }
+
   const extract = async () => {
-    const command = new DetectTextCommand(params);
-
     try {
-      const data = await client.send(command);
+      const analyzeDoc = new AnalyzeDocumentCommand(params);
+      const data = await client.send(analyzeDoc);
+      const [key_map, value_map, block_map] = get_kv_map(data);
+      const kv_relations = get_kv_relationship(key_map, value_map, block_map);
+      let result = [];
 
-      setOcrResult(data.TextDetections?.map((d) => d.DetectedText).join(" "));
-      // process data.
+      for (const [key, value] of Object.entries(kv_relations)) {
+        console.log(`${key}: ${value}`);
+        result.push({ field: key.slice(0, -1), value: value[0] });
+      }
+      console.log(result);
+
+      setOcrResult(result);
     } catch (error) {
-      // error handling.
     } finally {
-      // finally.
     }
   };
 
@@ -99,7 +198,24 @@ const Home = () => {
                   padding: "10px",
                 }}
               >
-                {ocrResult}
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Campo</th>
+                      <th>Valor</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {ocrResult.map((entry, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{entry.field}</td>
+                          <td>{entry.value}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
               </Text>
             </Stack>
           )}
